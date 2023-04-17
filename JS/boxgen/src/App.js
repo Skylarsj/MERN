196881e1097e3adb1c5d07918a802d0dc4a3623e
Skylarsj <skylarsj@icloud.com>
2023-04-17 12:38:57 -0700

    Update

--- JS/boxgen/src/App.js
+++ JS/boxgen/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Color from './components/boxGen';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1 className="text-3xl font-bold">
+      Hello world!
+      </h1>
+      <Color/>
+    </div>
   );
 }
 
--- JS/boxgen/src/components/boxGen.js
+++ JS/boxgen/src/components/boxGen.js
@@ -1,1 +1,29 @@
-import react from 'react';
+import React, {useState} from 'react';
+
+const Color = (props) => {
+    
+    const [color, setColor] = useState({
+        color: ""
+    });
+
+    const creatingColor = (e) => {
+        setColor({
+            ...color,
+            [e.targe.name]: e.target.value
+        })
+    };
+    return(
+        <div>
+            <form onSubmit={ creatingColor }>
+                <div>
+                    <label>First Name: </label> 
+                    <input class="w-30 m-3 border border-sky-500" id="color" name="color" type="text" onChange={creatingColor} />
+                </div>
+            </form>
+        </div>
+        );
+};
+
+
+
+export default Color;
