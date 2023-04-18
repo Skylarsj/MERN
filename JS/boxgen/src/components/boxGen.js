d73e944b98779f6f16ceecbe8effd3c80aed4461
Skylarsj <skylarsj@icloud.com>
2023-04-17 20:26:39 -0700

    Update

--- JS/boxgen/src/components/boxGen.js
+++ JS/boxgen/src/components/boxGen.js
@@ -3,7 +3,9 @@ import React, {useState} from 'react';
 const Color = (props) => {
     
     const [color, setColor] = useState({
-        color: ""
+        color: "",
+        width: "",
+        height: ""
     });
 
     const creatingColor = (e) => {
@@ -17,6 +19,8 @@ const Color = (props) => {
             <form onSubmit={ creatingColor }>
                 <div>
                     <input class="w-30 h-8 m-3 p-3 border border-sky-500" id="color" name="color" type="text" onChange={creatingColor} placeholder="Color"/>
+                    <input class="w-30 h-8 m-3 p-3 border border-sky-500" id="width" name="width" type="text" onChange={creatingColor} placeholder="width"/>
+                    <input class="w-30 h-8 m-3 p-3 border border-sky-500" id="height" name="height" type="text" onChange={creatingColor} placeholder="height"/>
                 <button class="rounded-full w-20 border border-sky p-1 bg-sky-300">Submit</button>
                 </div>
             </form>
