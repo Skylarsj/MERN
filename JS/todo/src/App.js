1fb4f46a9ead9516cf70dd2c306b65c5c9ebf428
Skylarsj <skylarsj@icloud.com>
2023-04-20 23:01:14 -0700

    update

--- JS/todo/src/App.js
+++ JS/todo/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <h1 className="font-bold flex justify-center">To Do List</h1>
       <Todo list={list} setList={setList}/>
-      <List list={list} setLis={setList}/>
+      <List list={list} setList={setList}/>
     </div>
   );
 }
--- JS/todo/src/components/done.jsx
+++ JS/todo/src/components/done.jsx
@@ -1,27 +1,31 @@
+import classNames from 'classnames';
 import React from 'react';
+
+let style = classNames('flex items-center h-10 w-full rounded-md m-3 font-bold pl-3 bg-sky-500 hover:bg-sky-700'
+    );
 
 const List = ({list, setList}) => {
 
-    const testing = (item) => {
+    const done = (item) => {
+
+        if (item.done === true)
+
+        item.done = !item.done;
+        setList([...list]);
         
-        console.log(item.id)
-        
+        style = classNames('flex items-center h-10 w-full rounded-md m-3 font-bold pl-3 bg-blue-500 hover:bg-sky-700'
+        );
+        console.log(item)
     }
+
+
 
     return(
         <div className="flex w-64 flex-col-reverse mx-auto">
             {
                 list.map((item, index) => (
-                    <div onClick={() => testing(item)}
-                    key={index} className="
-                        flex
-                        items-center h-10 w-full
-                        rounded-md
-                        m-3
-                        bg-sky-500
-                        hover:bg-sky-700
-                        font-bold
-                        pl-3">
+                    <div onClick={() => done(item)}
+                    key={index} className={item.done ? 'flex items-center h-10 w-full rounded-md m-3 font-bold pl-3 bg-green-500 hover:bg-sky-700' : ''}>
                             {item.name}
                     </div>
                 ))
@@ -32,3 +36,4 @@ const List = ({list, setList}) => {
 }
 
 export default List;
+
