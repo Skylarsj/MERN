75c039197aeb47ee138fa850ca39fad4bf3151bb
Skylarsj <skylarsj@icloud.com>
2023-04-21 10:16:15 -0700

    Update

--- JS/todo/src/components/done.jsx
+++ JS/todo/src/components/done.jsx
@@ -5,33 +5,29 @@ const List = ({list, setList}) => {
 
     const done = (item) => {
 
-        if (item.done === true)
-            return;
-
         item.done = !item.done;
         setList([...list]);
-        
-
-        console.log(item)
-        console.log(list)
     }
 
-
+    const remove = (item) => {
+        const updatedList = list.filter(listItem => listItem !== item);
+        setList(updatedList);
+    }
 
     return(
         <div className="flex w-64 flex-col-reverse mx-auto">
             {
                 list.map((item, index) => (
-                <div>
+                <div className="flex">
                     <div onClick={() => done(item)}
                         key={index} className={item.done ? 'flex items-center justify-between h-10 w-full rounded-md mt-5 font-bold pl-3 bg-green-500' : 'flex items-center h-10 w-full rounded-md mt-5 font-bold pl-3 bg-sky-500 hover:bg-sky-700'}>
                         {item.name}
+                    </div>
                         {item.done && (
-                            <button className="flex justify-center text-xs font-thin items-center text-center p-2 rounded-full border-2 border-[#fa8072] mb-8 bg-rose-700">
+                            <button onClick={() => remove(item)} className="flex justify-center text-xs font-thin items-center text-center p-2 rounded-md mt-5 ml-1 bg-rose-700">
                             Delete
                             </button>
                         )}
-                    </div>
                 </div>
                 ))
             }
