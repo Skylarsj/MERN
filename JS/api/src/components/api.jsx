6a32f370b31d6465ac20ca53b90eb74b62098ab2
Skylarsj <skylarsj@icloud.com>
2023-04-22 18:11:15 -0700

    Update

--- JS/api/src/components/api.jsx
+++ JS/api/src/components/api.jsx
@@ -1,2 +1,47 @@
-import React from' react';
+import React, {useState, useEffect} from 'react';
 
+const Api = () => {
+
+    const [Pokemon, setPokemon] = useState([]);
+
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon")
+        .then(response => {
+        // not the actual JSON response body but the entire HTTP response
+        return response.json();
+        }).then(response => {
+        // we now run another promise to parse the HTTP response into usable JSON
+        console.log(response);
+        }).catch(err=>{
+        console.log(err);
+        });
+    }, []);
+    
+    return (
+        <div className="container">
+
+        </div>
+    );
+    
+}
+export default Api
+
+// const Example = (props) => {
+//     const [people, setPeople] = useState([]);
+
+//     useEffect(() => {
+//         fetch('https://swapi.dev/api/people/')
+//             .then(response => response.json())
+//             .then(response => setPeople(response.results))
+//     }, []);
+
+//     return (
+//         <div>
+//             {people.length > 0 && people.map((person, index)=>{
+//                 return (<div key={index}>{person.name}</div>)
+//             })}
+//         </div>
+//     );
+// }
+// export default Example;
