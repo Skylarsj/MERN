ee1397fc7286de9a50b845aad93d6ee623a2ad30
Skylarsj <skylarsj@icloud.com>
2023-04-22 18:57:31 -0700

    Finished With PokeAPI

--- JS/api/src/components/api.jsx
+++ JS/api/src/components/api.jsx
@@ -1,16 +1,17 @@
 import React, {useState, useEffect} from 'react';
-
+import logo from './pokeapi.png'
 const Api = () => {
 
-    const [Pokemon, setPokemon] = useState([]);
+    const [pokemon, setPokemon] = useState([]);
 
 
     useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon")
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
         .then(response => {
         // not the actual JSON response body but the entire HTTP response
         return response.json();
         }).then(response => {
+            setPokemon(response.results)
         // we now run another promise to parse the HTTP response into usable JSON
         console.log(response);
         }).catch(err=>{
@@ -19,8 +20,15 @@ const Api = () => {
     }, []);
     
     return (
-        <div className="container">
-
+        <div className="mx-auto w-full">
+            <div className="flex bg-[#EF5350] w-full h-15">
+                <img clasName="pl-10"src={logo} alt="logo"/>
+            </div>
+                <div className="body font-bold text-lg">
+                    {pokemon.map((pokemon, index)=>{
+                        return(<li key={index}>{pokemon.name}</li>)
+                    })}
+                </div>
         </div>
     );
     
