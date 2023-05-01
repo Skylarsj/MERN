b0550223fc1e05f53a3d4f27b36afa4647be4749
Skylarsj <skylarsj@icloud.com>
2023-05-01 15:08:40 -0700

    Update

--- JS/faker_api/server.js
+++ JS/faker_api/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-
 const {faker} = require('@faker-js/faker');
+
 const createUser = ()=>{
+
     const newUser = {
-        passwoed: faker.internet.password(),
+        id: faker.datatype.uuid(),
+        password: faker.internet.password(),
         email: faker.internet.email(),
         phoneNumber: faker.phone.number(),
         firstName: faker.name.firstName(),
@@ -16,6 +18,7 @@ const createUser = ()=>{
 
 const createCompany = ()=>{
     const newCompany = {
+        id: faker.datatype.uuid(),
         name: faker.company.name(),
         street: faker.address.street(),
         city: faker.address.city(),
@@ -23,13 +26,29 @@ const createCompany = ()=>{
         zipCode: faker.address.zipCode(),
         country: faker.address.country()
     }
+    return newCompany
 }
 app.use(express.json() );
 app.use(express.urlencoded({extended:true}));
 
-app.get("api/users", (req, res) => {
+app.get("/api/users/new", (req, res) => {
     const newUser = createUser();
     res.json(newUser)
 });
+
+app.get("/api/company/new", (req, res) => {
+    const newComapny = createCompany();
+    res.json(newComapny)
+});
+
+app.get("/api/user/company", (req, res) => {
+    const comp = createCompany();
+    const use = createUser();
+    const allThem = {
+        user: use,
+        company: comp
+    };
+    res.json(allThem)
+})
 
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
