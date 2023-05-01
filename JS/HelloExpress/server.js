88c966899e796dddbd2a0500d8a7b75eb8116a87
Skylarsj <skylarsj@icloud.com>
2023-05-01 10:51:59 -0700

    Update

--- JS/HelloExpress/server.js
+++ JS/HelloExpress/server.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-// req is short for request
-// res is short for response
+const users = [
+  { firstName: "Reimu",  lastName: "Hakurei"    },
+  { firstName: "Marisa", lastName: "Kirisame"   },
+  { firstName: "Sanae",  lastName: "Kochiya"    },
+  { firstName: "Sakuya", lastName: "Izayoi"     },
+  { firstName: "Momiji", lastName: "Inubashiri" }
+];
+  
+
+// make sure these lines are above any app.get or app.post code blocks
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello World" });
+  res.json(users);
 });
 
 // this needs to be below the other code blocks
