48db36e1efef060642e9ec155f1ce4dab9041a74
Skylarsj <skylarsj@icloud.com>
2023-05-01 12:32:28 -0700

    Update

--- JS/faker_api/server.js
+++ JS/faker_api/server.js
@@ -2,4 +2,28 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+const {faker} = require('@faker-js/faker');
+const createUser = ()=>{
+    let id = 0;
+    const newUser = {
+        passwoed: faker.internet.password(),
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.number(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        id: id
+    }
+    (id + 1);
+};
+
+const createCompany = ()=>{
+    const newCompany = {
+        name: faker.company.name(),
+        street: faker.address.street(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        zipCode: faker.address.zipCode(),
+        country: faker.address.country()
+    }
+}
 app.use(express.json() );
