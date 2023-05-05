e4bc406d6d7c97eb85f1fd40cd86e387d4087a08
Skylarsj <skylarsj@icloud.com>
2023-05-05 14:39:56 -0700

    Update

--- JS/myNewProject/server/server.js
+++ JS/myNewProject/server/server.js
@@ -1,5 +1,5 @@
+require('./routes/person.routes')(app); 
 const express = require('express');
 const app = express();
 const port = 8000;
-    
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
