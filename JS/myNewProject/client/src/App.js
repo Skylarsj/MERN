c06b5ec16e565073e20236c2dcbd1441e04a782f
Skylarsj <skylarsj@icloud.com>
2023-05-09 09:45:44 -0700

    Update

--- JS/myNewProject/client/src/App.js
+++ JS/myNewProject/client/src/App.js
@@ -1,12 +1,19 @@
-import PersonForm from './components/PersonFrom';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <PersonForm/>
-    </div>
-  );
+import React, { useState } from 'react';
+import axios from 'axios';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Main from '../views/Main';
+// import PersonForm from '../components/PersonForm';
+// import PersonList from '../components/PersonList';
+const App = () => {
+    
+    return(
+	<div>
+    	<BrowserRouter>
+            <Routes>
+	    <Route element={<Main/>} path="/home" default /> //adding the default makes this the default path
+            </Routes>
+    	</BrowserRouter>
+        </div>
+    ) 
 }
-
 export default App;
--- JS/myNewProject/client/src/components/PersonFrom.js
+++ JS/myNewProject/client/src/components/PersonFrom.js
@@ -1,40 +1,22 @@
 import React, { useState } from 'react'
 import axios from 'axios';
-const PersonForm = () => {
-    //keep track of what is being typed via useState hook
+const PersonForm = (props) => {
+    const {people, setPeople} = props; //this is new
     const [firstName, setFirstName] = useState(""); 
     const [lastName, setLastName] = useState("");
-    //handler when the form is submitted
     const onSubmitHandler = (e) => {
-        //prevent default behavior of the submit
         e.preventDefault();
-        //make a post request to create a new person
         axios.post('http://localhost:8000/api/people', {
-            firstName,    // this is shortcut syntax for firstName: firstName,
-            lastName      // this is shortcut syntax for lastName: lastName
+            firstName,    
+            lastName   
         })
             .then(res=>{
-                console.log(res); // always console log to get used to tracking your data!
+                console.log(res); 
                 console.log(res.data);
+                // we will update the lifted state of our people array 
+                //    to include the current value in state plus the single 
+                //    new object created and returned from our post request. 
+                setPeople([...people, res.data]); //this is new
             })
             .catch(err=>console.log(err))
-    }
-    
-    return (
-        <form onSubmit={onSubmitHandler}>
-            <p>
-                <label>First Name</label><br/>
-                {/* When the user types in this input, our onChange synthetic event 
-                    runs this arrow function, setting that event's target's (input) 
-                    value (what's typed into the input) to our updated state   */}
-                <input type="text" onChange = {(e)=>setFirstName(e.target.value)}/>
-            </p>
-            <p>
-                <label>Last Name</label><br/>
-                <input type="text" onChange = {(e)=>setLastName(e.target.value)}/>
-            </p>
-            <input type="submit"/>
-        </form>
-    )
-}
-export default PersonForm;
+    }       // **** Our return/form will remain unchanged!
