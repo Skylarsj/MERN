608990948fb35eeba03aaf70a5486e0124bd7108
Skylarsj <skylarsj@icloud.com>
2023-05-09 14:26:53 -0700

    Update

--- JS/ProductManager/client/src/components/Product.jsx
+++ JS/ProductManager/client/src/components/Product.jsx
@@ -10,20 +10,22 @@ const ProductForm = () => {
     });
 
     const onSubmitHandler = (e) => {
-        e.preventDefault();
+
 
         axios.post('http://localhost:8000/api/product', formInput )
-
-            .then(res=>{
-                console.log(res);
-                console.log(res.data);
-            })
-            .catch(err=>console.log(err));
-        setFormInput({
-            Title: "",
-            Price: "",
-            Description: ""
+        .then(res=>{
+            console.log(res);
+            console.log(res.data);
         })
+        .catch(err=>console.log(err));
+
+
+        
+            setFormInput({
+                Title: "",
+                Price: "",
+                Description: ""
+            })
     }
 
     const onChangeHandler = (e) => {
@@ -36,11 +38,12 @@ const ProductForm = () => {
         <div className="flex justify-center flex-col border-2 h-[250px]">
             <p className="mb-5">Add Product</p>
             <form onSubmit={onSubmitHandler}>
-                <input placeholder="Title" className="border mb-5" type="text" name="Title" value={formInput.Title} onChange={onChangeHandler}/>
+                
+                <input placeholder="Title" className="border mb-5" type="text" name="Title" value={formInput.Title} onChange={onChangeHandler} required/>
 
-                <input placeholder="$00.0" className="border mb-5"type="number" min="0.00" step="0.01" name="Price" value={formInput.Price} onChange={onChangeHandler}/>
+                <input placeholder="$00.0" className="border mb-5"type="number" min="0.00" step="0.01" name="Price" value={formInput.Price} onChange={onChangeHandler} required/>
 
-                <input placeholder="Description" className="border mb-5" type="text" name="Description" value={formInput.Description} onChange={onChangeHandler}/>
+                <input placeholder="Description" className="border mb-5" type="text" name="Description" value={formInput.Description} onChange={onChangeHandler} required/>
 
                 <button className="border w-[225px] hover:bg-slate-400 active:bg-slate-700" type="submit">Submit</button>
             </form>
