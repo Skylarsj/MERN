bc9aaa4c8dabbd3ea7a4d50070765b6451b55c7c
Skylarsj <skylarsj@icloud.com>
2023-05-11 17:30:16 -0700

    Update

--- JS/ProductManager/client/src/components/Detail.jsx
+++ JS/ProductManager/client/src/components/Detail.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import {Link} from 'react-router-dom';
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
 const Detail = (props) => {
     const [product, setProduct] = useState({})
     const {id} = useParams(); 
+    const navigate = useNavigate();
     useEffect(() => {
         axios.get("http://localhost:8000/api/product/" + id)
             .then( res => {
@@ -14,12 +15,24 @@ const Detail = (props) => {
             })
             .catch( err => console.log(err) );
     }, [id]);
+
+
+    const deleteProduct = (id)  => {
+        axios.delete('http://localhost:8000/api/product/' + id)
+        .then(res => {
+            navigate('/product/');
+        })
+        .catch(err => console.log(err))
+        
+        
+    }
     return (
         <div className="w-[300px] m-auto border">
             <p className="flex justify-center py-5">Title: {product.Title}</p>
             <p className="flex justify-center pb-5">Price: ${product.Price}</p>
             <p className="flex justify-center pb-5">Description: {product.Description}</p>
             <Link className=" border-t flex justify-center h-10 hover:bg-green-500 pt-2"to={`/product/edit/${product._id}`}>Edit</Link>
+            <button className="w-full p-2 hover:bg-rose-600" onClick={(e)=>{deleteProduct(product._id)}}>Delete</button>
         </div>
     );
 }
--- JS/ProductManager/client/src/components/ProductList.jsx
+++ JS/ProductManager/client/src/components/ProductList.jsx
@@ -31,10 +31,10 @@ const ProductList = (props) => {
             {
                 product.slice(0).reverse().map((product, index)=>{
                     return (
-                        <div key={index} className="border hover:bg-slate-500 justify-between">
+                        <div key={index} className="border hover:bg-slate-500 justify-between mb-5">
                             <Link className="flex justify-center h-10 w-full p-2"to={`/product/${product._id}`}>See more: {product.Title}</Link>
                             <div>
-                                <button className="w-full p-2 hover:bg-rose-600" onClick={(e)=>{deleteProduct(product._id)}}>Delete</button>
+                                <button className="w-full p-2 hover:bg-rose-600 border-t" onClick={(e)=>{deleteProduct(product._id)}}>Delete</button>
                             </div>
                         </div>
                     )
