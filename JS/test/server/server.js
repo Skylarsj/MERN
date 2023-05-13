e22e1c41240d2b5e9b83db270074f2f4ffa1e9c8
Skylarsj <skylarsj@icloud.com>
2023-05-13 13:26:55 -0700

    Update

---
+++ JS/test/server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+require('./config/mongoose.config');
+
+const AllRoutes = require('ROUTES PATH GOES HERE');
+AllRoutes(app);
+
+app.listen(8000, () => {
+    console.log("Listening at Port 8000")
+})
